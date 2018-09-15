b513b312a29ab38db512c6668495c13cd142abd4
Lakics Nora <lakicsnora@Lakics-Air.home>
2018-09-16 00:51:50 +0200

    Added images to slides

--- src/button.js
+++ src/button.js
@@ -1,13 +1,13 @@
 function clickButton() {
-    document.getElementById("projects-button").addEventListener("click", function(){
-            document.getElementById("project-container").classList.add("show-projects");
-            document.getElementById("slide").classList.remove("show-projects");
-            document.getElementById("projects-button").style.display="none";
-            document.getElementById("projects-button").classList.add("next-button");
-            document.getElementById("container-bgd").style.background = "none";
-            document.getElementById("site-name").classList.add("main-title");
-            document.getElementById("footer-main").style.display="none";
-        });
+        document.getElementById("projects-button").addEventListener("click", function(){
+        document.getElementById("project-container").classList.add("show-projects");
+        document.getElementById("slide").classList.remove("show-projects");
+        document.getElementById("projects-button").style.display="none";
+        document.getElementById("projects-button").classList.add("next-button");
+        document.getElementById("container-bgd").style.background = "none";
+        document.getElementById("site-name").classList.add("main-title");
+        document.getElementById("footer-main").style.display="none";
+    });
 }
 
 module.exports = clickButton;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require("webpack-merge");
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
-//const svgFragments = require( 'postcss-svg-fragments' );
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
 
@@ -12,26 +11,20 @@ module.exports = ({ mode, presets } = {
         rules: [{
             test: /\.scss$/,
             use: [
-                // fallback to style-loader in development
                 process.env.NODE_ENV !== 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
                 "style-loader",
                 "css-loader",
                 "postcss-loader",
                 "sass-loader"
             ],
-        }
-        
+        }   
     ]
-    },
+},
     presets: [] }) => 
-    webpackMerge(
+        webpackMerge(
         {
-          mode,
-        //   plugins: [new HtmlWebpackPlugin({title: 'Lanora Design', template: './src/index.html'},
-        //   ),
-        //      new webpack.ProgressPlugin()],
-
-        plugins: [
+            mode,
+            plugins: [
             new webpack.ProvidePlugin({
                 $: 'jquery',
                 jQuery: 'jquery'
@@ -49,15 +42,10 @@ module.exports = ({ mode, presets } = {
                 template: './src/about.html',
             }),
               new MiniCssExtractPlugin({
-                // Options similar to the same options in webpackOptions.output
-                // both options are optional
                 filename: "style.scss"
-                //chunkFilename: "[id].css"
             }),
             new BaseHrefWebpackPlugin({ baseHref: '/' })
-    
           ],
-    },
-        
-        modeConfig(mode)
-    );
+    }, 
+    modeConfig(mode)
+);
